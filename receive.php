a1073c347e8f67c60f479ece4c81f8e9d3569897
Matthew Phillips <dutchmvp@hotmail.co.uk>
2013-10-24 19:41:18 +0100

    [MP] further testing

--- receive.php
+++ receive.php
@@ -4,5 +4,8 @@
 Welcome <?php echo $_GET["to"]; ?><br>
 Your email address is: <?php echo $_GET["from"]; ?>
 <p>http://hackman.j.layershift.co.uk/receive.php?to=447860033014&from=441234567890&content=Hello+World&msg_id=AB_12345</p>
+
+<p>Version 0.1</p>
+
 </body>
 </html>
