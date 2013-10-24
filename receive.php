feadb3a6657ec5ad721d5aec3b01c24c500ae4f8
Matthew Phillips <dutchmvp@hotmail.co.uk>
2013-10-24 19:31:33 +0100

    [MP] receive.php added

---
+++ receive.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+<p>http://hackman.j.layershift.co.uk/receive.php?to=447860033014&from=441234567890&content=Hello+World&msg_id=AB_12345</p>
+</body>
+</html>
