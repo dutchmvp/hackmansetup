4cfab01e2f2e8849f6f38d1cb6bd92cdf0d53de1
Lee Cooper <lee-cooper@hotmail.co.uk>
2013-10-26 14:05:45 +0100

    LC - front end setup atructure

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hack Manchester 2013 Setup</title>
+	</head>
+	<body>
+		<h1>This is a placeholder page</h1>
+		<h2>for our dev environment</h2>
+		<p>update v0.3</p>
+	</body
+</html>
