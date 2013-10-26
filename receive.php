010d7f1c45412b8f7909e51cc0fdedbedf0e4add
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-10-26 23:14:00 +0100

    Now using the new Sms class on the receive page

--- receive.php
+++ receive.php
@@ -1,10 +1,6 @@
 <html>
 <body>
 
-<?php
-
-?>
-
 Phone To: <?php echo $_GET["to"]; ?><br>
 Phone From: <?php echo $_GET["from"]; ?><br>
 Phone From: <?php echo $_GET["content"]; ?><br>
@@ -12,8 +8,9 @@ Msg ID: <?php echo $_GET["msg_id"]; ?><br>
 
 <?php
 
-$API_KEY = "ad8684f58a1beb7266576cfeb45f5b622dbd4aa1";
- 
+require "class-Sms.php";
+$sms = new Sms();
+
 $fromNumber = $_GET["from"];
 $content = $_GET["content"];
 $emailAddress = 'hi@mattp.me';
@@ -22,30 +19,12 @@ $pos = strpos($content, " ");
 $handle = substr($content, 0, $pos);
 $messageBody = substr($content, $pos + 1);
 
-$con=mysqli_connect("109.109.137.143","root","uA8GTi23xD","tfu");
-// Check connection
+$con = mysqli_connect("109.109.137.143", "root", "uA8GTi23xD", "tfu");
+
 if (mysqli_connect_errno())
   {
-	  $errorMessage = "Failed to connect to MySQL: " . mysqli_connect_error();
-	  echo $errorMessage;
-	try
-	{
-	    $url = 'https://api.clockworksms.com/http/send.aspx';
-		$myvars = 'KEY=' . 'ad8684f58a1beb7266576cfeb45f5b622dbd4aa1' . '&to=' . '447446022999' . '&content=' . $errorMessage;
-
-		$ch = curl_init( $url );
-		curl_setopt( $ch, CURLOPT_POST, 1);
-		curl_setopt( $ch, CURLOPT_POSTFIELDS, $myvars);
-		curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-		curl_setopt( $ch, CURLOPT_HEADER, 0);
-		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-
-		$response = curl_exec( $ch );
-	}
-	catch (ClockworkException $e)
-	{
-	    echo 'Exception sending SMS: ' . $e->getMessage();
-	}
+		$errorMessage = "Failed to connect to MySQL: " . mysqli_connect_error();
+		$sms->send($fromNumber, $errorMessage);
   }
   
 $foundUser = false;
@@ -62,12 +41,13 @@ while ($rowUser = mysqli_fetch_array($resultUser))
   	
   	$result = mysqli_query($con, "SELECT * FROM Contacts WHERE UserID = '$userId' AND Handle='$handle'");
 
-	while($row = mysqli_fetch_array($result))
+	while ($row = mysqli_fetch_array($result))
 	  {
 	  	$foundHandle = true;
 	    $emailAddress = $row["EmailAddress"];
 	    $twitter = $row["twitter"];
 	    $phone = $row["phone"];
+	    
 	    mysqli_query(
 	    	$con,
 	    	"INSERT INTO RecipeTriggers " .
