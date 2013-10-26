e671a09e3b52c48876c4460c44e594f6f67d215f
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-10-26 22:45:21 +0100

    Will it work without a constructor ?

--- class-Sms.php
+++ class-Sms.php
@@ -7,14 +7,12 @@ class Sms {
 	const FROM = "TFU";
 	const API_KEY = "ad8684f58a1beb7266576cfeb45f5b622dbd4aa1";
 
-	public function __construct() {
-	}
+	//public function __construct() {
+	//}
 
 	public function send($to, $message) {
 		$api = new Clockwork(self::API_KEY);
 		$sms = array("from" => self::FROM, "to" => $to, "message" => $message);
-		//$api = new Clockwork("ad8684f58a1beb7266576cfeb45f5b622dbd4aa1");
-		//$sms = array("from" => "TFU", "to" => "$to", "message" => "$message");
 		$result = $api->send($sms);
 	}
 }
