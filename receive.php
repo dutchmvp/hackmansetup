54e9417deb5939102e393cab33d99f39c5177404
Matthew Phillips <dutchmvp@hotmail.co.uk>
2013-10-26 15:05:12 +0100

    [mp] Added DB Conntection

--- receive.php
+++ receive.php
@@ -5,6 +5,21 @@ Phone To: <?php echo $_GET["to"]; ?><br>
 Phone From: <?php echo $_GET["from"]; ?><br>
 Phone From: <?php echo $_GET["content"]; ?><br>
 Msg ID: <?php echo $_GET["msg_id"]; ?><br>
+
+<?php
+$con=mysqli_connect("109.109.137.143","root","uA8GTi23xD","tfu");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+mysqli_query($con,"INSERT INTO RecipeTriggers (Nickname, MessageText, EmailAddress)
+VALUES ('John', 'Hi I am John','hi@mattp.me')");
+
+mysqli_close($con);
+?>
+
 <p>http://hackman.j.layershift.co.uk/receive.php?to=447860033014&from=441234567890&content=Hello+World&msg_id=AB_12345</p>
 
 <p>Version 0.2</p>
