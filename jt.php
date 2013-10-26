716723920e44ec1c79697d11dd1dec724ee5f63e
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-10-26 22:07:27 +0100

    Crap!

--- jt.php
+++ jt.php
@@ -18,8 +18,14 @@ $result = $clockwork->send($message);
 */
 
 require "class-Sms.php";
+
+echo "Doing new Sms()" . "<br/>"
 $sms = new Sms();
+echo "After new Sms()" . "<br/>"
+
+echo "Doing $sms->send" . "<br/>"
 $sms->send("447546372748", "Test message from $sms.send()");
+echo "After $sms->send" . "<br/>"
 
 ?>
 
