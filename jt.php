b69b144dadd6ce11d56f361583c929fbcbdd0b42
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-10-26 20:27:17 +0100

    Test - sending an sms by Clockwork api

--- jt.php
+++ jt.php
@@ -5,9 +5,16 @@
 <h1>My first PHP page</h1>
 
 <?php
+
 require "jt2.php";
-require "class-Clockwork.php";
 echo $jtMessage;
+
+require "class-Clockwork.php";
+$key = "";
+$clockwork = new Clockwork($key);
+$message = array("to" => "447546372748", "message" => "Test message");
+$result = $clockwork->send($message);
+
 ?>
 
 </body>
