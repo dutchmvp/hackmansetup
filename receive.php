ac5f8b85511a3c8510dab95333019c3100bff32a
Matthew Phillips <dutchmvp@hotmail.co.uk>
2013-10-26 19:24:23 +0100

    changed require

--- receive.php
+++ receive.php
@@ -1,13 +1,14 @@
 <html>
 <body>
 
+<?php require 'class-Clockwork.php'; ?>
+
 Phone To: <?php echo $_GET["to"]; ?><br>
 Phone From: <?php echo $_GET["from"]; ?><br>
 Phone From: <?php echo $_GET["content"]; ?><br>
 Msg ID: <?php echo $_GET["msg_id"]; ?><br>
 
 <?php
-require 'class-Clockwork.php';
 
 $API_KEY = "ad8684f58a1beb7266576cfeb45f5b622dbd4aa1";
  
