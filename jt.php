a2200e46d16c31270cb8b4abf7bfe48ca49633db
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-10-26 23:47:04 +0100

    New Database class

--- jt.php
+++ jt.php
@@ -3,9 +3,22 @@
 <body>
 
 <?php
+/*
 require "class-Sms.php";
 $sms = new Sms();
 $sms->send("447546372748", "Test message from send()");
+*/
+
+require "database.php";
+$database = new Database();
+$userRow = $database->lookupPhoneNumber("447546372748");
+if (!is_null($userRow)) {
+	$contactRow = $database->lookupHandle($userRow, "@dog");
+	if (!is_null($contactRow)) {
+		$database->createRecipeTrigger($userRow, $contactRow, "This is a test message (1)");
+	}
+}
+
 ?>
 
 </body>
