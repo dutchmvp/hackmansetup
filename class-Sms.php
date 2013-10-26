4b9b3ba4913e2244fa59470b07707bdac95dac08
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-10-26 22:50:00 +0100

    Now we are getting somewhere with this php class stuff

--- class-Sms.php
+++ class-Sms.php
@@ -7,9 +7,6 @@ class Sms {
 	const FROM = "TFU";
 	const API_KEY = "ad8684f58a1beb7266576cfeb45f5b622dbd4aa1";
 
-	//public function __construct() {
-	//}
-
 	public function send($to, $message) {
 		$api = new Clockwork(self::API_KEY);
 		$sms = array("from" => self::FROM, "to" => $to, "message" => $message);
--- jt.php
+++ jt.php
@@ -2,25 +2,10 @@
 <html>
 <body>
 
-<h1>My first PHP page</h1>
-
 <?php
-
-require "jt2.php";
-echo $jtMessage;
-
-/*
-require "class-Clockwork.php";
-$key = "ad8684f58a1beb7266576cfeb45f5b622dbd4aa1";
-$clockwork = new Clockwork($key);
-$message = array("to" => "447546372748", "message" => "Test message");
-$result = $clockwork->send($message);
-*/
-
 require "class-Sms.php";
 $sms = new Sms();
 $sms->send("447546372748", "Test message from send()");
-
 ?>
 
 </body>
