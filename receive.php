39c259512e3a8347726c54f9faf783ccfcbceee6
Matthew Phillips <dutchmvp@hotmail.co.uk>
2013-10-26 14:05:37 +0100

    [MP] Start HackManchester

--- receive.php
+++ receive.php
@@ -1,11 +1,13 @@
 <html>
 <body>
 
-Welcome <?php echo $_GET["to"]; ?><br>
-Your email address is: <?php echo $_GET["from"]; ?>
+Phone To: <?php echo $_GET["to"]; ?><br>
+Phone From: <?php echo $_GET["from"]; ?><br>
+Phone From: <?php echo $_GET["content"]; ?><br>
+Msg ID: <?php echo $_GET["msg_id"]; ?><br>
 <p>http://hackman.j.layershift.co.uk/receive.php?to=447860033014&from=441234567890&content=Hello+World&msg_id=AB_12345</p>
 
-<p>Version 0.1</p>
+<p>Version 0.2</p>
 
 </body>
 </html>
