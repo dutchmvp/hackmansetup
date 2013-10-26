71b277685a8025dd119906d5b905716bd25477a5
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-10-26 21:25:58 +0100

    Testing new Sms class

---
+++ class-Sms.php
@@ -0,0 +1,17 @@
+<?php
+
+require "class-Clockwork.php";
+
+class Sms {
+
+	const FROM = "TFU";
+	const API_KEY = "ad8684f58a1beb7266576cfeb45f5b622dbd4aa1";
+
+	public function send($to, $message) {
+		$api = new Clockwork(self::API_KEY);
+		$sms = array("from" => self::FROM, "to" => $to, "message" => $message);
+		$result = $api->send($sms);
+	}
+}
+
+?>
--- jt.php
+++ jt.php
@@ -9,11 +9,17 @@
 require "jt2.php";
 echo $jtMessage;
 
+/*
 require "class-Clockwork.php";
 $key = "ad8684f58a1beb7266576cfeb45f5b622dbd4aa1";
 $clockwork = new Clockwork($key);
 $message = array("to" => "447546372748", "message" => "Test message");
 $result = $clockwork->send($message);
+*/
+
+require "class-Sms.php";
+$sms = new Sms();
+$sms.send("447546372748", "Test message from $sms.send()");
 
 ?>
 
