38ceef7d1bc36fc74e316f52b22714ad5a5eef39
Matthew Phillips <dutchmvp@hotmail.co.uk>
2013-10-27 11:03:02 +0000

    [mp] Styling changes

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 
 <?php include 'header.php'; ?>
 <?php include 'register.php'; ?>
-<?php include 'footer.php'; ?>
 
 
--- loginPage.php
+++ loginPage.php
@@ -2,6 +2,12 @@
 	
 	<form method="post" action="login.php">
 		
+		<div class="logo">
+		
+		</div>
+		
+		<h2>
+		
 		<input type="email" placeholder="Email Address" name="email"/>
 		<input type="password" placeholder="Password" name="password"/>
 		<span class="rightShift">
