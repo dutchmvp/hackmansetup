37a12588613aec93692f597403c6d9f13e026749
Matthew Phillips <dutchmvp@hotmail.co.uk>
2013-10-27 12:27:46 +0000

    Now sending a welcome message on successful signup

--- signup.php
+++ signup.php
@@ -1,5 +1,8 @@
 <?php 
 
+	require "class-Sms.php";
+	$sms = new Sms();
+	
     include 'database.php';
     $database = new Database();
 
@@ -10,28 +13,10 @@
         $phoneNumber = $_POST['phone'];
         $password = $_POST['password'];
         $database->createUser($nickname, $phoneNumber, $email, $password);
-        header("Location: profile.php"); 
+        $sms->send($phoneNumber, "Welcome to TFU! You have been registered successfully!");
+        header("Location: profile.php");
     }
     else {
     }
-/*
-    # search the database to see if the email has been taken or not 
-    $query = sprintf($db,"SELECT * FROM Users WHERE Email='%s' LIMIT 1",mysql_real_escape_string($_POST['email'])); 
-    $sql = mysqli_query($query); 
-    $row = mysqli_fetch_array($sql); 
-   
-    # set vars
-    $nickname = $_POST['nickname'];
-    $PhoneNumber = $_POST['phone'];
-    $Email = $_POST['email'];
-    $Password = $_POST['password'];
-
-    #add to db
-    mysqli_query($db,"INSERT INTO Users (Nickname,PhoneNumber, Email, Password) VALUES ('$nickname', '$PhoneNumber', '$Email', '$Password')");
-    $sql = mysqli_query($query); 
-    # Redirect the user to a login page 
-    header("Location: profile.php"); 
-    exit; 
-*/
 
 ?>
